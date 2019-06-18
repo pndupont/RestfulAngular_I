7c0262ce423b52d74f83640993e27d165ff97323
pndupont <pndupont@gmail.com>
2019-06-18 11:10:32 -0500

    first commit

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,6 +1,6 @@
 var tasks = require('../controllers/tasks.js');
 module.exports = function(app){
-    app.get('/', tasks.index);
+    app.get('/tasks', tasks.index);
     app.post('/new/', tasks.new);
     app.delete('/remove/:id', tasks.destroy);
     app.get('/:id', tasks.show);
